59c2bd857aed34eb466fbe4697feb9d3c4d714dd
moxybetter285 <moxybetter285@gmail.com>
2026-03-30 23:13:54 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ const webhook = new WebhookClient({ url: webhookUrl });
 client.on('clientReady', () => { console.log(`Logged in as ${client.user.tag}`); });
 client.on('guildMemberAdd', async (member) => {
   try {
-    await webhook.send({ embeds: [{ title: 'CORAL SMP', description: `Welcome <@${member.id}> to CoralSMP\n\n**IP** - coralsmp.net\n**PORT** - 19132\n**STORE** - https://coralsmp.tebex.io`, color: 5814783, thumbnail: { url: member.user.displayAvatarURL() }, footer: { text: `You are our ${member.guild.memberCount}th member!` } }] });
+    await webhook.send({ embeds: [{ 
+      title: 'CORAL SMP', 
+      description: `Welcome <@${member.id}> to CoralSMP\n\n**IP** - coralsmp.net\n**PORT** - 19132\n**STORE** - https://coralsmp.tebex.io`, 
+      color: 5814783, 
+      thumbnail: { url: member.user.displayAvatarURL() },
+      image: { url: 'https://raw.githubusercontent.com/moxybetter285/coral-bot/main/Koralski%20SMP%20logo%20u%20Minecraft%20stilu.png' },
+      footer: { text: `You are our ${member.guild.memberCount}th member!` } 
+    }] });
   } catch (err) { console.error('Failed:', err); }
 });
 client.login(token);
